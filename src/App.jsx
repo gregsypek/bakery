39d9dccddd350d91975ba53e73997d8007aa4139
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-12-14 20:00:18 +0100

    setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import { styled } from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import Products from "./pages/Products";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-	/* background-color: var(--color-brand-100); */
-	background-color: var(--color-black-900);
-
-	padding: 20px;
-`;
 
 function App() {
 	return (
 		<>
 			<GlobalStyles />
-			<StyledApp>
-				<Row type="horizontal">
-					<Heading as="h1">Bakery</Heading>
-
-					<div>
-						<Heading as="h3">Bakery</Heading>
-						<Button>Test</Button>
-						<Button variation="secondary">Test</Button>
-					</div>
-				</Row>
-				<Row type="vertical">
-					<Heading as="h3">Form</Heading>
-					<form>
-						<Input type="number" placeholder="Number of products" />
-					</form>
-				</Row>
-			</StyledApp>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Navigate replace to="dashboard" />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="orders" element={<Orders />} />
+					<Route path="products" element={<Products />} />
+					<Route path="users" element={<Users />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="account" element={<Account />} />
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,13 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+function Orders() {
+	return (
+		<Row type="horizontal">
+			<Heading as="h1">All Orders</Heading>
+			<p>Test</p>
+		</Row>
+	);
+}
+
+export default Orders;
