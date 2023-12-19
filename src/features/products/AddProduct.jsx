e663559d8bb15efe4b25b666dc67bfd66d92e4ad
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-12-19 09:29:04 +0100

    modal as compound component

--- src/features/products/AddProduct.jsx
+++ src/features/products/AddProduct.jsx
@@ -1,22 +1,43 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateProductForm from "./CreateProductForm";
+import ProductTable from "./ProductTable";
 
 function AddProduct() {
-	const [isOpenModal, setIsOpenModal] = useState(false);
 	return (
-		<div>
-			<Button onClick={() => setIsOpenModal((show) => !show)}>
-				Add new product
-			</Button>
-			{isOpenModal && (
-				<Modal onClose={() => setIsOpenModal(false)}>
-					<CreateProductForm onCloseModal={() => setIsOpenModal(false)} />
-				</Modal>
-			)}
-		</div>
+		<Modal>
+			<Modal.Open opens="product-form">
+				<Button>Add new product</Button>
+			</Modal.Open>
+			<Modal.Window name="product-form">
+				<CreateProductForm />
+			</Modal.Window>
+
+			<Modal.Open opens="table">
+				<Button>Show table</Button>
+			</Modal.Open>
+			<Modal.Window name="table">
+				<ProductTable />
+			</Modal.Window>
+		</Modal>
 	);
 }
+
+// function AddProduct() {
+// 	const [isOpenModal, setIsOpenModal] = useState(false);
+// 	return (
+// 		<div>
+// 			<Button onClick={() => setIsOpenModal((show) => !show)}>
+// 				Add new product
+// 			</Button>
+// 			{isOpenModal && (
+// 				<Modal onClose={() => setIsOpenModal(false)}>
+// 					<CreateProductForm onCloseModal={() => setIsOpenModal(false)} />
+// 				</Modal>
+// 			)}
+// 		</div>
+// 	);
+// }
 
 export default AddProduct;
